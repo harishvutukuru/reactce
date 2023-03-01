3467ab3e17a46487793890a4d093928436c26a67
harishvutukuru <harishvutukuru01@gmail.com>
2023-03-01 16:10:48 -0600

    commit10

---
+++ app1/src/components/childcomponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function childcomponent(props) {
+  return (
+    <div>
+        <button onClick={propos.greetparent}>Call the parent</button>
+    </div>
+  )
+}
+
+export default childcomponent
---
+++ app1/src/components/parentcomponent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class parentcomponent extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         parentname:"Parent";
+      }
+    }
+    greetparent(){
+        alert(`hello ${this.state.parentname}`)
+    }
+  render() {
+    return (
+      <div>parentcomponent</div>
+    )
+  }
+}
+
+export default parentcomponent
